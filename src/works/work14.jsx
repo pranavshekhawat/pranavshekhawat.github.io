946e45551c6f977f41b96ddbd94e234c13cf2a15
pranavshekhawat <pranav.shekhawat@nift.ac.in>
2023-07-04 00:37:54 +0530

    Cha cha changes

--- src/works/work14.jsx
+++ src/works/work14.jsx
@@ -16,168 +16,168 @@ function Work14() {
                        
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (2).jpg' alt="documant page 1" />
-                        <Img height="auto" src='\images\works\tarang\Img (3).jpg' alt="document page 2" />
+                        <Img height="auto" src='\images\works\tarang\img (2).jpg' alt="documant page 1" />
+                        <Img height="auto" src='\images\works\tarang\img (3).jpg' alt="document page 2" />
                     </div>
                     <br />
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (4).jpg' alt="documant page 3" />
-                        <Img height="auto" src='\images\works\tarang\Img (5).jpg' alt="document page 4" />
+                        <Img height="auto" src='\images\works\tarang\img (4).jpg' alt="documant page 3" />
+                        <Img height="auto" src='\images\works\tarang\img (5).jpg' alt="document page 4" />
                     </div>
                     <br />
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (6).jpg' alt="documant page 5" />
-                        <Img height="auto" src='\images\works\tarang\Img (7).jpg' alt="document page 6" />
+                        <Img height="auto" src='\images\works\tarang\img (6).jpg' alt="documant page 5" />
+                        <Img height="auto" src='\images\works\tarang\img (7).jpg' alt="document page 6" />
                     </div>
                     <br />
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (8).jpg' alt="documant page 7" />
-                        <Img height="auto" src='\images\works\tarang\Img (9).jpg' alt="document page 8" />
+                        <Img height="auto" src='\images\works\tarang\img (8).jpg' alt="documant page 7" />
+                        <Img height="auto" src='\images\works\tarang\img (9).jpg' alt="document page 8" />
                     </div>
                     <br />
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (10).jpg' alt="documant page 9" />
-                        <Img height="auto" src='\images\works\tarang\Img (11).jpg' alt="document page 10" />
+                        <Img height="auto" src='\images\works\tarang\img (10).jpg' alt="documant page 9" />
+                        <Img height="auto" src='\images\works\tarang\img (11).jpg' alt="document page 10" />
                     </div>
                     <br />
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (12).jpg' alt="documant page 11" />
-                        <Img height="auto" src='\images\works\tarang\Img (13).jpg' alt="document page 12" />
+                        <Img height="auto" src='\images\works\tarang\img (12).jpg' alt="documant page 11" />
+                        <Img height="auto" src='\images\works\tarang\img (13).jpg' alt="document page 12" />
                     </div>
                     <br />
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (14).jpg' alt="documant page 13" />
-                        <Img height="auto" src='\images\works\tarang\Img (15).jpg' alt="document page 14" />
+                        <Img height="auto" src='\images\works\tarang\img (14).jpg' alt="documant page 13" />
+                        <Img height="auto" src='\images\works\tarang\img (15).jpg' alt="document page 14" />
                     </div>
                     <br />
                     <span id="artisans" className="bookmark_positioner"></span>
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (16).jpg' alt="documant page 15" />
-                        <Img height="auto" src='\images\works\tarang\Img (17).jpg' alt="document page 16" />
+                        <Img height="auto" src='\images\works\tarang\img (16).jpg' alt="documant page 15" />
+                        <Img height="auto" src='\images\works\tarang\img (17).jpg' alt="document page 16" />
                     </div>
                     <br />
                     <span id="product_board" className="bookmark_positioner"></span>
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (18).jpg' alt="documant page 17" />
-                        <Img height="auto" src='\images\works\tarang\Img (19).jpg' alt="document page 18" />
+                        <Img height="auto" src='\images\works\tarang\img (18).jpg' alt="documant page 17" />
+                        <Img height="auto" src='\images\works\tarang\img (19).jpg' alt="document page 18" />
                     </div>
                     <br />
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (20).jpg' alt="documant page 19" />
-                        <Img height="auto" src='\images\works\tarang\Img (21).jpg' alt="document page 20" />
+                        <Img height="auto" src='\images\works\tarang\img (20).jpg' alt="documant page 19" />
+                        <Img height="auto" src='\images\works\tarang\img (21).jpg' alt="document page 20" />
                     </div>
                     <br />
                     <span id="theme" className="bookmark_positioner"></span>
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (22).jpg' alt="documant page 21" />
-                        <Img height="auto" src='\images\works\tarang\Img (23).jpg' alt="document page 22" />
+                        <Img height="auto" src='\images\works\tarang\img (22).jpg' alt="documant page 21" />
+                        <Img height="auto" src='\images\works\tarang\img (23).jpg' alt="document page 22" />
                     </div>
                     <br />
                     <span id="concepts" className="bookmark_positioner"></span>
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (24).jpg' alt="documant page 23" />
-                        <Img height="auto" src='\images\works\tarang\Img (25).jpg' alt="document page 24" />
+                        <Img height="auto" src='\images\works\tarang\img (24).jpg' alt="documant page 23" />
+                        <Img height="auto" src='\images\works\tarang\img (25).jpg' alt="document page 24" />
                     </div>
                     <br />
                       
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (26).jpg' alt="documant page 25" />
-                        <Img height="auto" src='\images\works\tarang\Img (27).jpg' alt="document page 26" />
+                        <Img height="auto" src='\images\works\tarang\img (26).jpg' alt="documant page 25" />
+                        <Img height="auto" src='\images\works\tarang\img (27).jpg' alt="document page 26" />
                     </div>
                     <br />
                       
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (28).jpg' alt="documant page 27" />
-                        <Img height="auto" src='\images\works\tarang\Img (29).jpg' alt="document page 28" />
+                        <Img height="auto" src='\images\works\tarang\img (28).jpg' alt="documant page 27" />
+                        <Img height="auto" src='\images\works\tarang\img (29).jpg' alt="document page 28" />
                     </div>
                     <br />
                       
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (30).jpg' alt="documant page 29" />
-                        <Img height="auto" src='\images\works\tarang\Img (31).jpg' alt="document page 30" />
+                        <Img height="auto" src='\images\works\tarang\img (30).jpg' alt="documant page 29" />
+                        <Img height="auto" src='\images\works\tarang\img (31).jpg' alt="document page 30" />
                     </div>
                     <br />
                       
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (32).jpg' alt="documant page 31" />
-                        <Img height="auto" src='\images\works\tarang\Img (33).jpg' alt="document page 32" />
+                        <Img height="auto" src='\images\works\tarang\img (32).jpg' alt="documant page 31" />
+                        <Img height="auto" src='\images\works\tarang\img (33).jpg' alt="document page 32" />
                     </div>
                     <br />
                       
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (34).jpg' alt="documant page 33" />
-                        <Img height="auto" src='\images\works\tarang\Img (35).jpg' alt="document page 34" />
+                        <Img height="auto" src='\images\works\tarang\img (34).jpg' alt="documant page 33" />
+                        <Img height="auto" src='\images\works\tarang\img (35).jpg' alt="document page 34" />
                     </div>
                     <br />
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (36).jpg' alt="documant page 35" />
-                        <Img height="auto" src='\images\works\tarang\Img (37).jpg' alt="document page 36" />
+                        <Img height="auto" src='\images\works\tarang\img (36).jpg' alt="documant page 35" />
+                        <Img height="auto" src='\images\works\tarang\img (37).jpg' alt="document page 36" />
                     </div>
                     <br />
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (38).jpg' alt="documant page 37" />
-                        <Img height="auto" src='\images\works\tarang\Img (39).jpg' alt="document page 38" />
+                        <Img height="auto" src='\images\works\tarang\img (38).jpg' alt="documant page 37" />
+                        <Img height="auto" src='\images\works\tarang\img (39).jpg' alt="document page 38" />
                     </div>
                     <br />
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (40).jpg' alt="documant page 39" />
-                        <Img height="auto" src='\images\works\tarang\Img (41).jpg' alt="document page 40" />
+                        <Img height="auto" src='\images\works\tarang\img (40).jpg' alt="documant page 39" />
+                        <Img height="auto" src='\images\works\tarang\img (41).jpg' alt="document page 40" />
                     </div>
                     <br />
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (42).jpg' alt="documant page 41" />
-                        <Img height="auto" src='\images\works\tarang\Img (43).jpg' alt="document page 42" />
+                        <Img height="auto" src='\images\works\tarang\img (42).jpg' alt="documant page 41" />
+                        <Img height="auto" src='\images\works\tarang\img (43).jpg' alt="document page 42" />
                     </div>
                     <br />
                     <span id="final_collection" className="bookmark_positioner"></span>
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (44).jpg' alt="documant page 43" />
-                        <Img height="auto" src='\images\works\tarang\Img (45).jpg' alt="document page 44" />
+                        <Img height="auto" src='\images\works\tarang\img (44).jpg' alt="documant page 43" />
+                        <Img height="auto" src='\images\works\tarang\img (45).jpg' alt="document page 44" />
                     </div>
                     <br />
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (46).jpg' alt="documant page 45" />
-                        <Img height="auto" src='\images\works\tarang\Img (47).jpg' alt="document page 46" />
+                        <Img height="auto" src='\images\works\tarang\img (46).jpg' alt="documant page 45" />
+                        <Img height="auto" src='\images\works\tarang\img (47).jpg' alt="document page 46" />
                     </div>
                     <br />
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (48).jpg' alt="documant page 47" />
-                        <Img height="auto" src='\images\works\tarang\Img (49).jpg' alt="document page 48" />
+                        <Img height="auto" src='\images\works\tarang\img (48).jpg' alt="documant page 47" />
+                        <Img height="auto" src='\images\works\tarang\img (49).jpg' alt="document page 48" />
                     </div>
                     <br />
                     <span id="student_profiles" className="bookmark_positioner"></span>
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (50).jpg' alt="documant page 49" />
-                        <Img height="auto" src='\images\works\tarang\Img (51).jpg' alt="document page 50" />
+                        <Img height="auto" src='\images\works\tarang\img (50).jpg' alt="documant page 49" />
+                        <Img height="auto" src='\images\works\tarang\img (51).jpg' alt="document page 50" />
                     </div>
                     <br />
 
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (52).jpg' alt="documant page 51" />
-                        <Img height="auto" src='\images\works\tarang\Img (53).jpg' alt="document page 52" />
+                        <Img height="auto" src='\images\works\tarang\img (52).jpg' alt="documant page 51" />
+                        <Img height="auto" src='\images\works\tarang\img (53).jpg' alt="document page 52" />
                     </div>
                     <br />
                     
                     <div className="overflowhidden  noflexinphone">
-                        <Img height="auto" src='\images\works\tarang\Img (54).jpg' alt="documant page 53" />
-                        <Img height="auto" src='\images\works\tarang\Img (55).jpg' alt="document page 54" />
+                        <Img height="auto" src='\images\works\tarang\img (54).jpg' alt="documant page 53" />
+                        <Img height="auto" src='\images\works\tarang\img (55).jpg' alt="document page 54" />
                     </div>
                 
 
